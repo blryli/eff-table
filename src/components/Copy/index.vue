0c1f3826462f81303b8203ed2193189ff1a660d1
张宁 <565713111@qq.com>
2021-04-12 15:59:32 +0800

    整理代码峰哥

--- src/components/Copy/index.vue
+++ src/components/Copy/index.vue
@@ -1,9 +1,8 @@
 <template>
-
+  <div />
 </template>
 
 <script>
-
 
 export default {
   data() {
@@ -12,42 +11,44 @@ export default {
     }
   },
   computed: {
-  
-  },
-  inject: ["table"],
-  methods: {
-    onCopy(e) {
-      if (!this.textArr.length) {
-        return true
-      }
 
-      let textArr = this.textArr.map(v => {
-        return v.join("\t ")
-      })
-      
-      let text = textArr.join("\r\n")
-      e.clipboardData.setData('text/plain', text);
-      this.table.$emit('copy')
-      e.preventDefault();
-    },
-    onListener() {
-      this.table.$on("select-range-data", (textArr) => {
-        this.textArr = textArr
-      })
-      document.addEventListener('copy', this.onCopy, false)
-    },
-    offListener() {
-      this.table.$off("select-range-data")
-     document.removeEventListener("copy", this.onCopy, false)
-    }
   },
+  inject: ['table'],
   mounted() {
     this.offListener()
     this.onListener()
   },
   destroyed() {
     this.offListener()
+  },
+  methods: {
+    onCopy(e) {
+      if (!this.textArr.length) {
+        return true
+      }
+
+      const textArr = this.textArr.map(v => {
+        return v.join('\t ')
+      })
+
+      const text = textArr.join('\r\n')
+
+      console.log(e, 1111)
+      e.clipboardData.setData('text/plain', text)
+      this.table.$emit('copy')
+      e.preventDefault()
+    },
+    onListener() {
+      this.table.$on('select-range-data', (textArr) => {
+        this.textArr = textArr
+      })
+      document.addEventListener('copy', this.onCopy, false)
+    },
+    offListener() {
+      this.table.$off('select-range-data')
+      document.removeEventListener('copy', this.onCopy, false)
+    }
   }
 }
 
-    </script>
+</script>
--- src/components/SelectRange/index.vue
+++ src/components/SelectRange/index.vue
@@ -1,20 +1,32 @@
-<template> </template>
+<template>
+  <div v-if="status >= 2" :style="toolStyle" class="tool">
+
+    <div class="sight">
+      <div class="vertical" />
+      <div class="lineae" />
+    </div>
+    <el-button :type="copyBtnType" size="mini" class="copybtn" @click="onClickCopy">复制</el-button>
+  </div>
+</template>
 
 <script>
+
 export default {
   data() {
     return {
       textArr: {},
       textMap: {},
       status: 0,
-      startPosition: {columnIndex: 0, rowIndex: 0},
-      endPosition: {columnIndex: 0, rowIndex: 0},
+      startPosition: { columnIndex: 0, rowIndex: 0 },
+      endPosition: { columnIndex: 0, rowIndex: 0 },
       unit: {
         width: 0,
-        height: 0,
+        height: 0
       },
       autoScrollIntervalId: null,
-      borderStyle: "solid"
+      borderStyle: 'solid',
+      copyBtnType: 'primary',
+      toolStyle: {}
     }
   },
   computed: {
@@ -22,159 +34,216 @@ export default {
   watch: {
     'table.scrollTop'(scrollTop, oldTop) {
       this.handleRect()
-      if (this.status != 1) {
-        return 
+      if (this.status !== 1) {
+        return
       }
-      
+
       this.endPosition.y += scrollTop - oldTop
-    },
+    }
   },
-  inject: ["table"],
+  inject: ['table'],
+  mounted() {
+    this.table.$on('table-mouse-up', this.selectRangeMouseUp)
+    this.table.$on('cell-mouse-down', this.selectRangeMouseDown)
+    this.table.$on('cell-mouse-move', this.selectRangeMouseMove)
+    this.table.$on('table-mouse-enter', this.selectRangeMouseEnter)
+    this.table.$on('table-mouse-leave', this.selectRangeMouseLeave)
+    this.table.$on('copy', () => {
+      if (this.status !== 2) {
+        return
+      }
+      this.borderStyle = 'dashed'
+      this.handleRect()
+    })
+
+    document.addEventListener('paste', this.onPaste, false)
+  },
+  destroyed() {
+    clearInterval(this.autoScrollIntervalId)
+    document.removeEventListener('paste', this.onPaste, false)
+  },
   methods: {
+    onClickCopy() {
+      this.copyBtnType = 'success'
+      document.execCommand('copy')
+    },
     changeData() {
       this.textArr = []
-      let {startRow, endRow, startColumn, endColumn} = this._getReac()
+      const { startRow, endRow, startColumn, endColumn } = this._getReac()
 
       let index = 0
 
       for (let i = startRow; i <= endRow; i++) {
         this.textArr.push([])
         for (let j = startColumn; j <= endColumn; j++) {
-          this.textArr[index].push(this.textMap[i+'-'+j])
+          this.textArr[index].push(this.textMap[i + '-' + j])
         }
         index++
       }
     },
-    selectRangeMouseEnter(e) {
-      if (this.status != 1) {
-        return 
-      }
+    sightMouseDown() {
+
     },
-    selectRangeMouseLeave(e) {
-      if (this.status != 1) {
-        return 
-      }
+    sightMouseUp() {
+
+    },
+    sightMouseMove() {
+
     },
     selectRangeMouseUp(e) {
+      const { endRow, endColumn } = this._getReac()
+      const map = this._getColumnMap()
+      const endColumnModel = map[endRow + '-' + endColumn]
+
+      if (endColumnModel) {
+        const columnEl = endColumnModel ? endColumnModel.$el : null
+        const rowEl = columnEl.parentElement
+        const offsetLeft = columnEl.offsetLeft + columnEl.offsetWidth
+        const offsetTop = rowEl.offsetTop + columnEl.offsetHeight * 2
+
+        this.toolStyle = { top: offsetTop - 7 + 'px', left: offsetLeft - 7 + 'px' }
+        console.log(offsetLeft, offsetTop, this.toolStyle, columnEl.style.height.replace('px', ''))
+      }
+
+      if (this.status < 2) {
+        this.copyBtnType = 'primary'
+      }
       this.status = 2
-      this.table.$emit("select-range-data", this.textArr)
+      this.handleRect()
+      this.table.$emit('select-range-data', this.textArr)
     },
     selectRangeMouseDown(e) {
-      this.borderStyle = "solid"
+      this.borderStyle = 'solid'
       this.status = 1
-      const {cell,rowIndex ,columnIndex} = e
-      const column = cell.parentNode
-      const row = column.parentNode
+      const { rowIndex, columnIndex } = e
 
-      this.endPosition = {rowIndex: rowIndex, columnIndex: columnIndex}
-      this.startPosition = {rowIndex: rowIndex, columnIndex: columnIndex}
-      
+      this.endPosition = { rowIndex: rowIndex, columnIndex: columnIndex }
+      this.startPosition = { rowIndex: rowIndex, columnIndex: columnIndex }
+
       this.textMap = {}
       this.textArr = []
-      
+
       this.handleRect()
       this.changeData()
     },
     selectRangeMouseMove(e) {
-      if (this.status != 1) {
-        return 
+      if (this.status !== 1) {
+        return
       }
-      const {cell,rowIndex ,columnIndex} = e
-      this.endPosition = {rowIndex: rowIndex, columnIndex: columnIndex}
-     
+      const { rowIndex, columnIndex } = e
+      this.endPosition = { rowIndex: rowIndex, columnIndex: columnIndex }
+
       this.handleRect()
       this.changeData()
     },
     _getReac() {
-      let startRow = this.endPosition.rowIndex > this.startPosition.rowIndex ? this.startPosition.rowIndex : this.endPosition.rowIndex
-      let endRow = this.endPosition.rowIndex < this.startPosition.rowIndex ? this.startPosition.rowIndex : this.endPosition.rowIndex
+      const startRow = this.endPosition.rowIndex > this.startPosition.rowIndex ? this.startPosition.rowIndex : this.endPosition.rowIndex
+      const endRow = this.endPosition.rowIndex < this.startPosition.rowIndex ? this.startPosition.rowIndex : this.endPosition.rowIndex
 
-      let startColumn = this.endPosition.columnIndex > this.startPosition.columnIndex ? this.startPosition.columnIndex : this.endPosition.columnIndex
-      let endColumn = this.endPosition.columnIndex < this.startPosition.columnIndex ? this.startPosition.columnIndex : this.endPosition.columnIndex
+      const startColumn = this.endPosition.columnIndex > this.startPosition.columnIndex ? this.startPosition.columnIndex : this.endPosition.columnIndex
+      const endColumn = this.endPosition.columnIndex < this.startPosition.columnIndex ? this.startPosition.columnIndex : this.endPosition.columnIndex
 
-      return {startRow, endRow, startColumn, endColumn}
+      return { startRow, endRow, startColumn, endColumn }
     },
-    handleRect() {
-      let {startRow, endRow, startColumn, endColumn} = this._getReac()
+    _getColumnMap() {
+      const rows = this.table.$refs.body.$children
 
-      let rows = this.table.$refs.body.$children
-      let map = {}
+      const map = {}
       rows.forEach(v => {
         v.$children.forEach(vv => {
-          map[vv.rowIndex+"-"+vv.columnIndex] = vv
+          map[vv.rowIndex + '-' + vv.columnIndex] = vv
           vv.style = {}
         })
       })
 
+      return map
+    },
+    handleRect() {
+      const { startRow, endRow, startColumn, endColumn } = this._getReac()
+
+      const map = this._getColumnMap()
+
       for (let i = startRow; i <= endRow; i++) {
         for (let j = startColumn; j <= endColumn; j++) {
-          let column = map[i+"-"+j]
-          let style = {}
+          const column = map[i + '-' + j]
+          const style = {}
 
-          if (j == startColumn) {
+          if (j === startColumn) {
             style.borderLeft = `2px ${this.borderStyle} #1177e8`
           }
 
-          if (j == endColumn) {
+          if (j === endColumn) {
             style.borderRight = `2px ${this.borderStyle} #1177e8`
           }
 
-          if (i == startRow) {
+          if (i === startRow) {
             style.borderTop = `2px ${this.borderStyle} #1177e8`
           }
 
-          if (i == endRow) {
+          if (i === endRow) {
             style.borderBottom = `2px ${this.borderStyle} #1177e8`
           }
 
           if (column) {
             column.style = style
-            this.textMap[i+'-'+j] = column.text
+            this.textMap[i + '-' + j] = column.text
           }
         }
       }
     },
     onPaste(e) {
-      if (this.status != 2) {
+      if (this.status !== 2) {
         return true
       }
 
-      let {startRow, startColumn} = this._getReac()
+      const { startRow, startColumn } = this._getReac()
 
       let data = e.clipboardData.getData('text/plain')
-      data = data.split("\n")
+      data = data.split('\n')
       data = data.map(v => {
-        if (v.indexOf("\t ") != -1) {
-          return v.split("\t ")
+        if (v.indexOf('\t ') !== -1) {
+          return v.split('\t ')
         }
 
-        return v.split("\t")
+        return v.split('\t')
       })
 
-      let postData = {startRow, startColumn, data}
-      this.table.$emit("table-paste", postData)
+      const postData = { startRow, startColumn, data }
+      this.table.$emit('table-paste', postData)
     }
-  },
-  mounted() {
-      this.table.$on("table-mouse-up", this.selectRangeMouseUp)
-      this.table.$on("cell-mouse-down", this.selectRangeMouseDown)
-      this.table.$on("cell-mouse-move", this.selectRangeMouseMove)
-      this.table.$on("table-mouse-enter", this.selectRangeMouseEnter)
-      this.table.$on("table-mouse-leave", this.selectRangeMouseLeave)
-      this.table.$on("copy", () => {
-        if (this.status != 2) {
-          return 
-        }
-        this.borderStyle = "dashed"
-        this.handleRect()
-      })
-
-    document.addEventListener('paste', this.onPaste, false)
-
-  },
-  destroyed() {
-    clearInterval(this.autoScrollIntervalId)
-    document.removeEventListener("paste", this.onPaste, false)
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.tool {
+  position: absolute;
+  display: flex;
+  .copybtn {
+    margin-left: 10px;
+  }
+  .sight {
+    width: 14px;
+    height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: crosshair;
+    div {
+      background-color: rgb(17, 119, 232);
+      position: absolute;
+      border-radius: 4px;
+    }
+    .vertical {
+      width: 4px;
+      height: 14px;
+    }
+
+    .lineae {
+      width: 14px;
+      height: 4px;
+      margin-top: -1px;
+    }
+  }
+}
+</style>
