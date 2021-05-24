e91478526e9385087e0d880f20d2a69cd2cb4fdd
陈志武 <378656992@qq.com>
2021-05-24 09:41:35 +0800

    commit

--- packages/layout/src/layout.js
+++ packages/layout/src/layout.js
@@ -2,17 +2,21 @@
 export default {
   // 页面布局容器组件
   name: 'layout',
+  props: ['span'],
   data() {
     return {}
   },
   methods: {
   },
   render(h) {
+    const spanCss = this.span ? `span-${this.span}` : ''
+
     return h(
       'div',
       {
         'class': {
-          'layout': true
+          'layout': true,
+          [spanCss]: !!this.span
         },
         on: {
           click: (e) => {
