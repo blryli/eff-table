b68ec299dc33c19917b693c772e2dfa53dac2ff9
张宁 <565713111@qq.com>
2021-05-31 19:18:51 +0800

    fixbug

--- docs-src/views/Base.vue
+++ docs-src/views/Base.vue
@@ -17,9 +17,7 @@
     <h2>Border 边框</h2>
     <p><span class="primary">border</span> 属性设置为<span class="primary"> true </span>显示竖边框</p>
     <section class="demo">
-      <div class="section-content">
-        <eff-table v-model="columns" :data="data" border />
-      </div>
+      <div class="section-content" />
     </section>
 
     <section class="snippets">
@@ -76,6 +74,7 @@ const snippet = [
   }
   `
 ]
+import mock from 'mockjs'
 export default {
   name: '',
   components: {
@@ -112,10 +111,23 @@ export default {
         }
       ],
       data: [
-        { name: '张三', sex: '男', phone: '13715201314' },
-        { name: '李四', sex: '男', phone: '13715201314' }
       ]
     }
+  },
+  mounted() {
+    this.loading = true
+    setTimeout(() => {
+      this.data = mock.mock({
+        'array|500': [
+          {
+            'name': '@city',
+            'sex': '@cfirst',
+            'phone': '@clast'
+          }
+        ]
+      }).array
+      this.loading = false
+    }, 1000)
   }
 }
 </script>
