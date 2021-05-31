8430b5d232e1e8208332f4ffcfc1f3cd11386bbe
张宁 <565713111@qq.com>
2021-05-31 10:54:01 +0800

    增加表单tooltip

--- packages/form/src/form-item.vue
+++ packages/form/src/form-item.vue
@@ -1,7 +1,36 @@
 <template>
-  <layout :span="form.isResponse ? 24 : span" :style="{'margin-left': '-' + form.itemGutter, 'margin-right': '-' + form.itemGutter}">
-    <div class="v-form-item" :class="{'is-required': required}" :style="{'--lineHeight': form.lineHeight}">
-      <label v-if="title" class="v-form-item__title" :style="{flex: `0 0 ${tWidth}`}">{{ title }}</label>
+  <layout
+    :span="form.isResponse ? 24 : span"
+    :style="{'margin-left': '-' + form.itemGutter, 'margin-right': '-' + form.itemGutter}"
+  >
+    <div
+      class="v-form-item"
+      :class="{'is-required': required}"
+      :style="{'--lineHeight': form.lineHeight}"
+    >
+      <div
+        :style="{flex: `0 0 ${tWidth}`, height: '32px'}"
+        class="flex align-center justify-end"
+        style=" position: relative;"
+      >
+        <template v-if="titlePrefix != null">
+          <i
+            class="v-form-item__tooltip v-form-item__ooh margin-right-xs"
+            @mouseenter="messageEnter"
+            @mouseleave="messageLeave"
+          >{{ titlePrefix.icon == 'ooh'? '!' : '?' }}</i>
+          <div v-if="showMessage" class="v-form-item__message_container">
+            <div class="v-form-item__message">{{ titlePrefix.message }}</div>
+          </div>
+
+        </template>
+        <label
+          v-if="title"
+          class="v-form-item__title"
+        >
+          {{ title }}
+        </label>
+      </div>
       <FormField
         class="v-form-item__content"
         :prop="prop"
@@ -15,19 +44,22 @@
 
 <script>
 import FormField from './form-field'
-
+import { render as Render } from 'core/render'
 export default {
   name: 'VFormItem',
-  components: { FormField },
+  components: { FormField, Render },
   props: {
     title: { type: String, default: '' },
     titleWidth: { type: String, default: '' },
     span: { type: Number, default: 24 },
     rules: { type: Array, default: () => [] },
-    prop: { type: String, default: '' }
+    prop: { type: String, default: '' },
+    titlePrefix: { type: Object, default: () => { return null } }
   },
   data() {
-    return {}
+    return {
+      showMessage: false
+    }
   },
   inject: ['form'],
   computed: {
@@ -37,6 +69,17 @@ export default {
     tWidth() {
       const { titleWidth, form } = this
       return titleWidth || form.titleWidth || '80px'
+    }
+  },
+  mounted() {
+    console.log(this.titlePrefix)
+  },
+  methods: {
+    messageEnter(e) {
+      this.showMessage = true
+    },
+    messageLeave(e) {
+      this.showMessage = false
     }
   }
 }
@@ -49,6 +92,53 @@ export default {
   flex-direction: row;
   align-items: flex-start;
   flex-wrap: nowrap;
+  &__tooltip {
+    transform: rotate(-10deg);
+        width: 14px;
+    height: 14px;
+    border-radius: 100%;
+    background-color: #666;
+    color: white;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__message_container {
+    position: absolute;
+    width: 2000%;
+    top: 0;
+    left: 0;
+  }
+  &__message {
+left: 17px;
+    top: -29px;
+    background: #666;
+    height: 50px;
+    position: absolute;
+    border-radius: 5px;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 26px;
+    font-size: 12px;
+    padding: 0 10px;
+    &::after {
+      content: "";
+    /* width: 10px; */
+    /* height: 10px; */
+    /* background-color: #666; */
+    position: absolute;
+    bottom: -14px;
+    z-index: 99;
+    left: 4px;
+    border: 7px solid transparent;
+    border-top: 7px solid #666;
+    }
+  }
+
   &::before {
     display: table;
     content: "";
@@ -61,7 +151,7 @@ export default {
   .vue-popover-trigger {
     position: relative;
   }
-  &.is-required>.v-form-item__title:before {
+  &.is-required .v-form-item__title:before {
     content: "*";
     line-height: var(--lineHeight);
     color: #f52b2b;
@@ -82,14 +172,15 @@ export default {
     position: relative;
     line-height: var(--lineHeight);
     font-size: 14px;
-    &::before, &::after{
+    &::before,
+    &::after {
       display: table;
       content: "";
     }
-    &::after{
+    &::after {
       clear: both;
     }
-    & :only-child{
+    & :only-child {
       width: 100%;
     }
   }
@@ -104,11 +195,13 @@ export default {
 }
 
 /* 响应式布局 */
-.v-form--title-top .v-form-item, .is-response .v-form-item {
+.v-form--title-top .v-form-item,
+.is-response .v-form-item {
   flex-direction: column;
 }
 
-.v-form--title-top .v-form-item__title, .is-response .v-form-item__title {
+.v-form--title-top .v-form-item__title,
+.is-response .v-form-item__title {
   flex: 1 !important;
   text-align: left;
   padding-right: 0;
