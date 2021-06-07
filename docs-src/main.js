768c203a2055f45fb981f87200da468006dae858
lizhili <405549416@qq.com>
2021-06-07 13:46:58 +0800

    fix

--- docs-src/main.js
+++ docs-src/main.js
@@ -7,11 +7,7 @@ import('../package.json').then(config => import(`../dist/${config.name}.css`))
 import 'element-ui/lib/theme-chalk/index.css'
 import "../packages/styles/layout.scss"
 import "../packages/styles/panel.scss"
-<<<<<<< HEAD
-Vue.use(Components, {renderMap: {'input': 'el-input'}})
-=======
 Vue.use(Components)
->>>>>>> ec97a06245dc28e244f2b67d60f7b76c6d734c21
 Vue.use(Element, { size: 'mini' })
 
 new Vue({
